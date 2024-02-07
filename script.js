eafbbe4fb3f4ee28f2f6097c5b0c465e7b0f9d87
Buchiano <buchiano89@gmail.com>
2024-02-07 12:33:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// alert(welcome to Awka);
+// document.write('welcome to Nigeria');
+console.log('today is a great day to code');
+var _peter ='welcome to a new era';
+console.log(_peter);
+var x=23;
+console.log(x);
+let g ='Buchi';
+let AlexJohnson=28;
+AlexJohnson=50;
+console.log(AlexJohnson);
+const Buchi=27;
+const a='How old is Buchi';
+const Buchi=27;
+console.log(a,Buchi);
+
+
+
+let email='buchiano89@gmail.com';
+let first_name='Buchi';
+const total=first_name+ '' + email 
+console.log(total);
+// full length of the character
+console.log(total.length);
+// to get the uppercase
+console.log(total.uppercase);
+let ninjas=total.toLowerCase();
+console.log[ninjas];
+console.log(ninjas,total.tolocaluppercase);
+
+
+const title ='one thing is to get to the top another thing is to stay at the top';
+const author = Buchi;
+const likes= 30;
+
+const solution ='the book called $(title) is written by $ (author) is spread to over $ (likes) country all over the world;
+console.log(solution);
+ let x=2;
+ let y=4;
+ const result=x+y;
+ console.log(result);
+ const result=y/x;
+ console.log(result);
+ 
+  
+  
+  
